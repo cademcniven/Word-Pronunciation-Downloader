2478533b643dd67b3cfcb9e2316c2263b030dabb
cademcniven <cademcniven@gmail.com>
2020-06-20 10:58:26 -0700

    uploaded code

---
+++ pronunciation.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def DownloadWord(word):
+    URL = 'https://www.lexico.com/en/definition/' + word
+    print(URL)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    parents = soup.find_all('h3', class_='pronunciations')
+
+    for element in parents:
+        link = element.find('audio')['src']
+        audio = requests.get(link, allow_redirects=True)
+        open(word + '.mp3', 'wb').write(audio.content)
+
+inputFile = 'input.txt'
+with open(inputFile) as fp:
+    line = fp.readline()
+    while line:
+        DownloadWord(line.strip())
+        line = fp.readline()
+
+input()
